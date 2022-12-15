b89f0b5351c22447c099e6e3026401d76767668c
12uche <uchibuezevictor@gmail.com>
2022-12-15 11:36:39 +0100

    New codes added to my proect file

--- JS/index.js
+++ JS/index.js
@@ -4,18 +4,18 @@ const nav = document.querySelector('.nav');
 const navBar = document.getElementById("nav");
 const logo1 = document.getElementById('logo1')
 const logo = document.getElementById("logo")
-console.log(123);
+console.log(125);
 window.onscroll = e => {
   const offset = window.scrollY;
-  offset > 50 ? nav.classList.add("scrolled"): 
+  offset > 55 ? nav.classList.add("scrolled"): 
   navBar.classList.remove("scrolled");
-  offset > 50 ? logo1.classList.remove("d-none"):
+  offset > 55 ? logo1.classList.remove("d-none"):
   logo1.classList.add("d-none");
-  offset > 50 ? document.getElementById('space').classList.add("d-none"):
+  offset > 55 ? document.getElementById('space').classList.add("d-none"):
   document.getElementById('space').classList.remove("d-none");
-  offset > 50 ? navLink.classList.remove("link"):
+  offset > 55 ? navLink.classList.remove("link"):
   navLink.classList.add("link");
-  offset < 20? logo.classList.remove("img"):
+  offset < 200? logo.classList.remove("img"):
   logo.classList.add("img")
 } 
  
@@ -31,20 +31,20 @@ const lname = document.getElementById('lastname');
 const eMail = document.getElementById('email');
 const mEssage = document.getElementById('textarea');
 
-suBtn.onclick = () => {
-  if(fname.value == '') {
-    document.querySelector('.req').style.display="block"
-  }else document.querySelector('.req').style.display="none"
-  if(lname.value == '') {
-    document.querySelector('.req1').style.display="block"
-  }else document.querySelector('.req1').style.display="none"
-  if(eMail.value == '') {
-    document.querySelector('.req2').style.display="block"
-  }else document.querySelector('.req2').style.display="none"
-  if(mEssage.value == '') {
-    document.querySelector('.req3').style.display="block"
-  }else document.querySelector('.req3').style.display="none"
-}
+// suBtn.onclick = () => {
+//   if(fname.value == '') {
+//     document.querySelector('.req').style.display="block"
+//   }else document.querySelector('.req').style.display="none"
+//   if(lname.value == '') {
+//     document.querySelector('.req1').style.display="block"
+//   }else document.querySelector('.req1').style.display="none"
+//   if(eMail.value == '') {
+//     document.querySelector('.req2').style.display="block"
+//   }else document.querySelector('.req2').style.display="none"
+//   if(mEssage.value == '') {
+//     document.querySelector('.req3').style.display="block"
+//   }else document.querySelector('.req3').style.display="none"
+// }
 
 
  
